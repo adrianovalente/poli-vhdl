dca679754c6e08f35283587c949d8eea41b81d80
Adriano Valente <dri9595@gmail.com>
2015-11-23 19:35:22 -0200

    Refactoring edge_detector

--- edge_detector.vhd
+++ edge_detector.vhd
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-entity custom_edge_detector is
+entity edge_detector is
       port ( clk         : in  STD_LOGIC;
              signal_in   : in  STD_LOGIC;
              output      : out  STD_LOGIC
       );
-end custom_edge_detector;
+end edge_detector;
 
-architecture arch of custom_edge_detector is
+architecture arch of edge_detector is
      signal signal_d : STD_LOGIC;
 begin
     process(clk)
@@ -16,5 +16,5 @@ begin
                signal_d <= signal_in;
          end if;
     end process;
-    output <= (not signal_d) and signal_in; 
+    output <= (not signal_d) and signal_in;
 end arch;
